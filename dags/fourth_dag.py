e31c5c9417b07554a4a44bd72a92afcf1ed404e0
Gus1101 <augustosantosb60@gmail.com>
2024-11-11 11:31:55 -0300

    changes made in fourth_dag

--- dags/fourth_dag.py
+++ dags/fourth_dag.py
@@ -12,13 +12,13 @@ dag = DAG(
 
 task1 = BashOperator(task_id="task1", bash_command="sleep 5", dag=dag)
 task2 = BashOperator(task_id="task2", bash_command="sleep 5", dag=dag)
-task2 = BashOperator(task_id="task3", bash_command="sleep 5", dag=dag)
-task2 = BashOperator(task_id="task4", bash_command="sleep 5", dag=dag)
-task2 = BashOperator(task_id="task5", bash_command="sleep 5", dag=dag)
-task2 = BashOperator(task_id="task6", bash_command="exit 1", dag=dag)
-task2 = BashOperator(task_id="task7", bash_command="sleep 5", dag=dag)
-task2 = BashOperator(task_id="task8", bash_command="sleep 5", dag=dag)
-task3 = BashOperator(task_id="task9", bash_command="sleep 5", dag=dag,
+task3 = BashOperator(task_id="task3", bash_command="sleep 5", dag=dag)
+task4 = BashOperator(task_id="task4", bash_command="sleep 5", dag=dag)
+task5 = BashOperator(task_id="task5", bash_command="sleep 5", dag=dag)
+task6 = BashOperator(task_id="task6", bash_command="exit 1", dag=dag)
+task7 = BashOperator(task_id="task7", bash_command="sleep 5", dag=dag)
+task8 = BashOperator(task_id="task8", bash_command="sleep 5", dag=dag)
+task9 = BashOperator(task_id="task9", bash_command="sleep 5", dag=dag,
                      trigger_rule = "one_failed")
 
 # Exploring Precedence
