51ef0617a5ec2e225eb8610ec230ec353cff5c2e
Gus1101 <augustosantosb60@gmail.com>
2024-11-11 15:34:56 -0300

    refactorin second_dag to use context management

--- dags/second_dag.py
+++ dags/second_dag.py
@@ -2,17 +2,17 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-dag = DAG(
+with DAG(
     dag_id="second_dag",
     description="my second dag",
     start_date=datetime(2024, 11, 9),
     schedule="@daily",
     catchup=False
-)
+) as dag:
 
-task1 = BashOperator(task_id="task1", bash_command="sleep 5", dag=dag)
-task2 = BashOperator(task_id="task2", bash_command="sleep 5", dag=dag)
-task3 = BashOperator(task_id="task3", bash_command="sleep 5", dag=dag)
+    task1 = BashOperator(task_id="task1", bash_command="sleep 5")
+    task2 = BashOperator(task_id="task2", bash_command="sleep 5")
+    task3 = BashOperator(task_id="task3", bash_command="sleep 5")
 
-# Using parallelism
-task1 >> [task2, task3]
+    # Using parallelism
+    task1 >> [task2, task3]
