b8f7e72eb949608f31070d41edb4a9a1f5d0fda1
Gus1101 <augustosantosb60@gmail.com>
2024-11-10 22:53:27 -0300

    setting trigger dag to fail

--- dags/trigger_dag_1.py
+++ dags/trigger_dag_1.py
@@ -10,7 +10,7 @@ dag = DAG(
     catchup=False
 )
 
-task1 = BashOperator(task_id="task1", bash_command="sleep 5", dag=dag)
+task1 = BashOperator(task_id="task1", bash_command="exit 1", dag=dag)
 task2 = BashOperator(task_id="task2", bash_command="sleep 5", dag=dag)
 task3 = BashOperator(task_id="task3", bash_command="sleep 5", dag=dag,
                      trigger_rule = "one_failed") # Task 3 must not be executed
